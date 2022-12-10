7c6c582e3df69e35a5a7b1087c7462b66145090c
Mfkfkfkdkduo <120150333+Mfkfkfkdkduo@users.noreply.github.com>
2022-12-10 10:52:45 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -44,8 +44,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "candy music")
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "1548904516").split())
-)  # Input type must be interger
+    map(int, getenv("OWNER_ID", "1548904516").split()))  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
